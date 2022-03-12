e6109e86d55b40c69b858722f6d4e0f4f9c6a0d7
Jesus Galan Lopez (yy) <yiyu.jgl@gmail.com>
2022-03-12 20:47:26 +0100

    monadic and dyadic functions

--- rpn.js
+++ rpn.js
@@ -11,12 +11,14 @@ const Stack = document.getElementById('stack');  let mod = Empty; //mode
 const Imm = document.getElementById('imm');      let imm = false; //immediate
 const Help = document.getElementById('help');    let cur = "";    //current
 const Log = document.getElementById('log');      let ans = "";    //last answer
+                                                 let sel = 0;     //selection
 
 // stack
-function input(c) { cur += c; mod = Input; }
+function input(c) { cur += c; mod = Input; sel = 1; }
 function push(t, v) {
 	if (t != Exp && v < 0) t = Exp; // neg values are not editable!
 	stk.push({type: t, value: v}); mod = (t == Exp ? Eval : t == Ans ? Input : Empty);
+	sel = Math.min(stk.length, 2);
 }
 function push_curr() {
 	if (stk.length == 1 && stk[0].type == Ans) { stk[0].type = Val; mod = Empty; }
@@ -31,10 +33,15 @@ function reset() { clear(); ans = Done.innerHTML = Stack.innerHTML = Log.innerHT
 function id(x) { return x; }
 function swap() { if ((n = push_curr()) < 2) return; stk[n-1] = id(stk[n-2], stk[n-2] = stk[n-1]); }
 function over() { if ((n = push_curr()) < 3) return; stk[n-1] = id(stk[n-3], stk[n-3] = stk[n-2], stk[n-2] = stk[n-1]); }
+function select() {
+	if (cur != "") { push_curr(); return; }
+	sel = (sel <= 1 ? Math.min(stk.length, 2) : sel - 1);
+}
 
 // functions
 function monadic(f) { if (push_curr() < 1) return; x = pop_exp(); push(Exp, f + x); if(imm) evaluate(); }
 function dyadic(f) { if (push_curr() < 2) return; w = pop_exp(); x = pop_exp(); push(Exp, x + f + w); if(imm) evaluate(); }
+function ambval(m, d) { if (sel == 1) monadic(m); else if (sel == 2) dyadic(d); }
 function evaluate() { if (push_curr() < 1) return; x = pop().value; add_done(ans = String(eval(x)), x); push(Ans, ans); }
 function immediate() { imm = !imm; Imm.className = (imm ? "imm" : "");  }
 
@@ -42,21 +49,21 @@ function immediate() { imm = !imm; Imm.className = (imm ? "imm" : "");  }
 function keydown(e) {
 	//Log.textContent += ` ${e.code}`;
 	if (e.shiftKey) switch (e.code) {
-		case "Equal": dyadic('+'); break;
-		case "Minus": monadic('-'); break;
-		case "Digit8": dyadic('*'); break;
-		case "Slash": monadic('1/'); break;
+		case "Equal": ambval('1+', '+'); break;
+		case "Digit8": ambval('2*', '*'); break;
+		case "Space": immediate(); break;
 		case "Tab": e.preventDefault(); over(); break;
 		case "Backspace": e.preventDefault(); drop(); break;
+		case "Escape": e.preventDefault(); reset(); break;
 		case "Enter": e.preventDefault(); dup(); break;
 		default: return;
 	} else switch (e.code) {
 		case "Digit0": case "Digit1": case "Digit2": case "Digit3": case "Digit4": case "Digit5":
 		case "Digit6": case "Digit7": case "Digit8": case "Digit9": input(e.code[e.code.length-1]); break;
 		case "Period": input('.'); break;
-		case "Minus": dyadic('-'); break;
-		case "Slash": dyadic('/'); break;
-		case "Space": push_curr(); break;
+		case "Minus": ambval('-', '-'); break;
+		case "Slash": ambval('1/', '/'); break;
+		case "Space": select(); break;
 		case "Tab": e.preventDefault(); swap(); break;
 		case "Escape": e.preventDefault(); clear(); break;
 		case "Enter": e.preventDefault();
@@ -94,7 +101,7 @@ function update() {
 	Stack.innerHTML = ''; n = stk.length;
 	if (c = Done.lastChild) c.className = (cur == "" && ans != "" && n == 0 ? "ans" : "none");
 	if (cur != "" && n++ == 0) { element(X, cur); cursor(Input); return; }
-	for (const e of stk) { c = (n == 2 ? W : n == 1 ? X : U); element(c, e.value); n--;	}
+	for (const e of stk) { element(n > sel ? U : n == 2 ? W : X, e.value); n--;	}
 	if (cur != "") element(X, cur);
 	cursor(mod); window.scrollTo(0, document.body.scrollHeight);
 }
