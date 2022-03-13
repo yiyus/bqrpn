4b6127636f4399a3169c2f2065b4ea118365543f
Jesus Galan Lopez (yy) <yiyu.jgl@gmail.com>
2022-03-13 14:18:06 +0100

    results stack

--- rpn.js
+++ rpn.js
@@ -1,45 +1,62 @@
-// rpn +-*/ calculator. JGL (yy). 2022
+// bqrpn
+// © JGL (yy). 2022
 
-// modes                        // args                     // types
-const Empty = Symbol("empty");  const X = Symbol("x");      const Val = Symbol("value");
-const Input = Symbol("input");  const W = Symbol("w");      const Ans = Symbol("answer");
-const Eval = Symbol("eval");    const U = Symbol("unsel");  const Exp = Symbol("expression");
+// args                     // types
+const X = Symbol("x");      const Val = Symbol("val");
+const W = Symbol("w");      const Res = Symbol("res");
+const U = Symbol("unsel");  const Exp = Symbol("exp");
 
 // doc elements                                      // globals
-const Done = document.getElementById('done');        let stk = [];    //stack
-const Stack = document.getElementById('stack');      let mod = Empty; //mode
+const Results = document.getElementById('results');  let stk = [];    //stack
+const Stack = document.getElementById('stack');      let mod = Val;   //mode
 const Imm = document.getElementById('imm');          let imm = false; //immediate
 const Help = document.getElementById('help');        let cur = "";    //current
-const Helping = document.getElementById('helping');  let ans = "";    //last answer
-const Log = document.getElementById('log');          let sel = 0;     //selection
+const Helping = document.getElementById('helping');  let sel = 0;     //selection
+const Log = document.getElementById('log');          let res = -1;    //selected result
 
 // stack
-function input(c) { cur += c; mod = Input; sel = 1 + (stk.length > 0); }
-function push(t, v) {
-	stk.push({type: t, value: v}); mod = (t == Exp ? Eval : t == Ans ? Input : Empty);
-	sel = Math.min(stk.length, 2);
-}
-function push_curr() {
-	if (stk.length == 1 && stk[0].type == Ans) { stk[0].type = Val; mod = Empty; }
-	if (cur != "") push(Val, cur); cur = ""; return stk.length;
-}
-function pop() { if (stk.length > 1) mod = (stk[stk.length-2] == Exp ? Eval : Empty); return stk.pop(); }
+function input(c) { cur += c; set_result(); sel = 1 + (stk.length > 0); }
+function push(t, v) { stk.push({type: t, value: v}); set_mod(t); sel = Math.min(stk.length, 2); }
+function push_curr() { if (cur != "") push(Val, cur); cur = ""; return stk.length; }
+function pop() { if ((n = stk.length) > 1) set_mod(stk[n-2].type); return stk.pop(); }
 function pop_exp() { e = pop(); return (e.type == Exp ? `(${e.value})` : e.value); }
-function dup() { if (push_curr() < 1) return; stk.push(stk[stk.length-1]); }
-function drop() { if (push_curr() < 1) return; pop(); }
-function clear() { stk = []; cur = ""; mod = Empty; }
-function reset() { clear(); ans = Done.innerHTML = Stack.innerHTML = Log.innerHTML = ""; }
+function dup() { if ((n = push_curr()) < 1) return; stk.push(stk[n-1]); }
+function drop() { if ((n = push_curr()) < 1) return; r = stk[n-1].type; pop(); set_result(is_result(r) ? r : n > 1 ? -1 : nres() - 1); }
+function clear() { stk = []; cur = ""; set_result(nres() - 1); sel = 0; }
+function reset() { clear(); set_result(); Results.innerHTML = Stack.innerHTML = Log.innerHTML = ""; }
 function id(x) { return x; }
-function swap() { if ((n = push_curr()) < 2) return; stk[n-1] = id(stk[n-2], stk[n-2] = stk[n-1]); }
-function over() { if ((n = push_curr()) < 3) return; stk[n-1] = id(stk[n-3], stk[n-3] = stk[n-2], stk[n-2] = stk[n-1]); }
+function swap() { if ((n = push_curr()) < 2) return; stk[n-1] = id(stk[n-2], stk[n-2] = stk[n-1]); set_result(); }
+function over() { if ((n = push_curr()) < 3) return; stk[n-1] = id(stk[n-3], stk[n-3] = stk[n-2], stk[n-2] = stk[n-1]); set_result(); }
 function selection() { if (cur != "") push_curr(); sel = (sel <= 1 ? Math.min(stk.length, 2) : sel - 1); }
 
 // functions
 function monadic(f) { if (push_curr() < 1) return; x = pop_exp(); push(Exp, f + x); if(imm) evaluate(); }
 function dyadic(f) { if (push_curr() < 2) return; w = pop_exp(); x = pop().value; push(Exp, w + f + x); if(imm) evaluate(); }
 function ambval(m, d, s = false) { if (sel == 1) monadic(m); if (sel < 2) return; if (s) swap(); dyadic(d); }
-function evaluate() { if (push_curr() < 1) return; x = pop().value; add_done(ans = fmt(bqn(x)), x); push(Ans, ans); }
+function evaluate() { if (push_curr() < 1) return; x = pop().value; push(push_result(ans = fmt(bqn(x)), x), ans); }
 function immediate() { imm = !imm; Imm.className = (imm ? "on" : "off");  }
+
+// results (ro stack)
+function nres() { return Results.childElementCount; }
+function push_result(r, x = "") {
+	tr = document.createElement("tr"); tr.appendChild(html("td", "", r));
+	if (x != "") tr.appendChild(html("td", Exp.description, "= " + x));
+	Results.appendChild(tr); return nres() - 1;
+}
+function set_result(i = -1) {
+	res = -1; for (const c of Results.childNodes) if (c.firstChild) c.firstChild.className = "";
+	if (i >= 0 && i < nres()) { Results.childNodes.item(res = i).firstChild.className = Res.description; }
+	set_mod();
+}
+function is_result(t) { return typeof(t) == "number"; }
+function set_mod(t = stk) {
+	if (cur != "") return mod = Val; if (res >= 0) return mod = Res;
+	if (t == stk) { if ((n = t.length) < 1) return mod = Val; t = t[n-1].type; }
+	return mod = t;
+}
+function prev_result() { push_curr(); if (res < 0) res = nres(); set_result(--res); }
+function next_result() { push_curr(); if (++res >= nres()) res = -1; set_result(res); }
+function res2tos() { if (res < 0 || res >= nres()) return; push(res, Results.childNodes.item(res).firstChild.innerHTML); }
 
 // input
 function keydown(e) {
@@ -60,7 +77,7 @@ function keydown(e) {
 		case "Backslash": ambval('|', '|', true); break;
 		case "Space": immediate(); break;
 		case "Tab": e.preventDefault(); over(); break;
-		case "Backspace": e.preventDefault(); drop(); break;
+		case "Backspace": e.preventDefault(); drop(); if (is_result(mod)) set_result(mod); break;
 		case "Escape": e.preventDefault(); reset(); break;
 		case "Enter": e.preventDefault(); dup(); break;
 		case "Slash": help(); break;
@@ -79,22 +96,20 @@ function keydown(e) {
 		case "Minus": ambval('-', '-', true); break;
 		case "Slash": ambval('÷', '÷', true); break;
 		case "Space": selection(); break;
+		case "ArrowUp": prev_result(); break;
+		case "ArrowDown": next_result(); break;
 		case "Tab": e.preventDefault(); swap(); break;
 		case "Escape": e.preventDefault(); clear(); break;
 		case "Enter": e.preventDefault();
 			if (cur != "") { push_curr(); break; }
-			if (stk.length < 1) { if (ans != "") push(Ans, ans); break; }
-			if (stk.length == 1 && stk[0].type == Ans) { drop(); break; }
-			if (mod == Eval) { evaluate(); break; }
-			add_done(ans = pop().value); break;
-		case "Backspace": e.preventDefault();
-			if (cur == "") {
-				if ((n = stk.length) < 1) break;
-				if (mod == Input && stk[n-1].type == Exp) { mod = Eval; break; }
-				if (mod == Eval) { drop(); break; }
-				cur = pop().value; mod = Input; break;
-			}
-			cur = cur.slice(0, cur.length - 1); mod = (cur != "" ? Input : Empty); break;
+			if (mod == Exp) { evaluate(); break; }
+			if (res >= 0) { res2tos(); set_result(); break; }
+			push_result(pop().value); set_result(); break;
+		case "Backspace": e.preventDefault(); n = stk.length;
+			if (cur != "") { cur = cur.slice(0, cur.length - 1); set_mod(); break; }
+			if (n < 1) break; if (mod == Val && stk[n-1].type == Exp) { mod = Exp; break; }
+			if (stk[n-1].type == Val) { input(pop().value); break; }
+			m = mod; drop(); if (is_result(m)) set_result(m); break;
 		default: return;
 	}
 	update()
@@ -105,20 +120,13 @@ function html(tag, cls, txt) {
 	e = document.createElement(tag); e.className = cls;
 	e.appendChild(document.createTextNode(txt)); return e;
 }
-function add_done(ans, x = "") {
-	tr = document.createElement("tr"); tr.appendChild(html("td", "ans", ans));
-	if (x != "") tr.appendChild(html("td", "exp", "= " + x));
-	Done.appendChild(tr);
-}
-function element(type, txt) { Stack.appendChild(html("span", type.description, txt)); }
-function cursor(mod) { Stack.appendChild(html("span", mod.description + (cur != "" ? " cursor" : ""), "█")); }
+function element(type, txt) { return Stack.appendChild(html("span", type.description, txt)); }
+function cursor(mod) { Stack.appendChild(html("cursor", (is_result(mod) ? Res : mod).description + (cur != "" ? " tight" : ""), "█")); }
 function update() {
 	Stack.innerHTML = ''; n = stk.length;
-	if (c = Done.lastChild) c.className = (cur == "" && ans != "" && n == 0 ? "ans" : "none");
-	if (cur != "" && n++ == 0) { element(X, cur); cursor(Input); return; }
+	if (cur != "" && n++ == 0) { element(X, cur); cursor(Val); return; }
 	for (const e of stk) { element(n > sel ? U : (n == 1 && sel == 2) ? W : X, e.value); n--; }
-	if (cur != "") element(sel > 1 ? W : X, cur);
-	cursor(mod); window.scrollTo(0, document.body.scrollHeight);
+	if (cur != "") element(sel > 1 ? W : X, cur); cursor(mod); window.scrollTo(0, document.body.scrollHeight);
 }
 function help() {
 	h = (Help.style.display == "block");
