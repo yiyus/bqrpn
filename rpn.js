b7b69be92f4afb17febfd0cecaa0eb70c3fa5724
Jesus Galan Lopez (yy) <yiyu.jgl@gmail.com>
2022-03-15 20:00:59 +0100

    remove modes, use st()

--- rpn.js
+++ rpn.js
@@ -16,19 +16,19 @@ const Help = document.getElementById('help');
 const Helping = document.getElementById('helping');
 const History = document.getElementById('history');
 const Err = document.getElementById('err');
-// globals: stack, mode, immediate, current, selection, result, mod-1, vars
-let stk = [], mod = Val, imm = true, cur = "", sel = 0, res = -1, md1 = "", vres = [];
+// globals: stack, immediate, current, selection, result, mod-1, vars, backspace
+let stk = [], imm = true, cur = "", sel = 0, res = -1, md1 = "", vres = [], back = false;
 
 // stack
 function ss() { return stk.length; } // stack size
-function st(i) { return stk[stk.length-i].type } // stack (element) type
+function st(i = 1) { return i > stk.length ? Val : stk[stk.length-i].type } // stack (element) type
 function input(c) { cur += c; setres(); sel = 1 + (ss() > 0); }
-function push(t, v) { stk.push({type: t, value: v}); setmod(t); sel = Math.min(ss(), 2); }
+function push(t, v) { stk.push({type: t, value: v}); sel = Math.min(ss(), 2); }
 function pushc() { if (cur != "") push(Val, cur); cur = ""; return ss(); }
-function pop() { if (ss() > 1) setmod(st(2)); return stk.pop(); }
+function pop() { return stk.pop(); }
 function popx() { e = pop(); return (e.type == Exp ? `(${e.value})` : e.value); }
 function dup() { if (pushc() < 1) return; stk.push(stk[ss()-1]); }
-function drop() { if (n = pushc() < 1) return; t = st(1); pop(); setres(isres(t) ? t : -1); sel = Math.min(ss(), sel); }
+function drop() { if (n = pushc() < 1) return; t = st(); pop(); setres(isres(t) ? t : -1); sel = Math.min(ss(), sel); }
 function clear() { stk = []; cur = ""; setres(-1); sel = 0; }
 function reset() { clear(); setres(); vres = []; Results.innerHTML = Stack.innerHTML = Err.innerHTML = ""; }
 function id(x) { return x; }
@@ -38,8 +38,8 @@ function selection() { setres(); if (cur != "") pushc(); sel = (sel == 1 ? Math.
 
 // functions
 function evaluate() { if (pushc() < 1) return; x = pop().value; ans = bqrpn(x); push(imm != Imm ? pushr(ans, x) : Exp, ans); }
-function monadic(f) { if (pushc() < 1) return; push(Exp, f + mod1() + popx()); if (imm && mod == Exp) evaluate(); }
-function dyadic(f) { if (pushc() < 2) return; push(Exp, popx() + f + mod1() + pop().value); if (imm && mod == Exp) evaluate(); }
+function monadic(f) { if (pushc() < 1) return; push(Exp, f + mod1() + popx()); if (imm && st() == Exp) evaluate(); }
+function dyadic(f) { if (pushc() < 2) return; push(Exp, popx() + f + mod1() + pop().value); if (imm && st() == Exp) evaluate(); }
 function ambval(m, d = null, s = false) { if (sel == 2 && d) { if (s) swap(); dyadic(d); } else if (sel >= 1) monadic(m); }
 function ambimm(m, d = null, s = false) { ps = sel; i = imm; imm = Imm; ambval(m, d, s); imm = i; sel = Math.min(ss(), ps); }
 function immediate() { imm = !imm; Imm.className = "toggle " + (imm ? "" : "none");  }
@@ -58,17 +58,11 @@ function setres(i = -1, sel = -1) {
 	for (const c of Results.childNodes) { c.className = ""; if (c.firstChild) c.firstChild.className = ""; }
 	if (i >= 0 && i < rs()) { getr(res = i).classList.add(Res.description); }
 	if (ss() > 1 && sel > 1 && isres(t = st(2))) getr(t).firstChild.classList.add(X.description);
-	if (ss() && sel > 0 && isres(t = st(1))) getr(t).firstChild.classList.add((sel > 1 ? W : X).description);
-	setmod();
+	if (ss() && sel > 0 && isres(st())) getr(st()).firstChild.classList.add((sel > 1 ? W : X).description);
 }
 function isres(t) { return typeof(t) == "number"; }
-function setmod(t = stk) {
-	if (cur != "") return mod = Val; if (res >= 0) return mod = Res;
-	if (t == stk) { if ((n = t.length) < 1) return mod = Val; t = t[n-1].type; }
-	return mod = t;
-}
-function prevres() { pushc(); if(res < 0 && isres(mod)) res = mod + 1; if (res < 0) res = rs(); setres(res - 1); }
-function nextres() { pushc(); if(res < 0 && isres(mod)) res = mod - 1; if (++res >= rs()) res = -1; setres(res); }
+function prevres() { pushc(); if(res < 0 && isres(st())) res = st() + 1; if (res < 0) res = rs(); setres(res - 1); }
+function nextres() { pushc(); if(res < 0 && isres(st())) res = st() - 1; if (++res >= rs()) res = -1; setres(res); }
 function rpush(r = null) { if(r === null) r = res; if (r >= 0) push(r, getr(r).firstChild.innerHTML); }
 
 // variables
@@ -77,6 +71,7 @@ function fetch(k) { if (!k in vres) return; rpush(vres[k]); }
 
 // input
 function keydown(e) { k = e.key;
+	back = back && e.key == "Backspace";
 	if (e.ctrlKey || e.altKey || e.metaKey) return; e.preventDefault();
 	if (md1 && (k == "Enter" || k == "Escape")) { mod1(); update(); return; }
 	if (md1 == "a←" || md1 == "a") {
@@ -147,17 +142,17 @@ function keydown(e) { k = e.key;
 		case "Enter":
 			if (e.shiftKey) { dup(); break; }
 			if (cur != "") { pushc(); break; }
-			if (mod == Exp) { evaluate(); break; }
-			if (isres(mod)) { drop(); break; }
 			if (res >= 0) { rpush(); setres(); break; }
+			if (isres(st())) { drop(); break; }
+			if (st() == Exp) { evaluate(); break; }
 			if (ss() > 0) pushr(pop().value); setres(); break;
 		case "Backspace":
 			if (!e.shiftKey) {
-				if (cur != "") { cur = cur.slice(0, cur.length - 1); setmod(); break; }
-				if (!ss()) break; if (mod == Val && ((t = st(1)) == Exp || t == Res)) { mod = t; break; }
-				if (st(1) == Val) { input(pop().value); break; }
+				if (cur != "") { cur = cur.slice(0, cur.length - 1); break; }
+				if (ss() && st() == Val) { input(pop().value); break; }
+				if (ss() && !back) { back = true; setres(); break; }
 			}
-			m = mod; drop(); break;
+			back = false; drop(); break;
 		default: return;
 	}
 	update()
@@ -169,8 +164,8 @@ function html(tag, cls, txt) {
 	e.appendChild(document.createTextNode(txt)); return e;
 }
 function element(type, txt) { return Stack.appendChild(html("span", type.description, txt)); }
-function cursor(mod) {
-	c = (res >= 0 ? Val : isres(mod) ? Res : mod).description + (cur != "" ? " tight" : "");
+function cursor(t) {
+	c = (res >= 0 ? Val : isres(t) ? Res : t).description + (cur != "" || back ? " tight" : "");
 	Stack.appendChild(html("cursor", (md1 ? c + " mod" : c), (res < 0 ? "█" : "⎕") + md1));
 }
 function update() {
@@ -182,7 +177,7 @@ function update() {
 	}
 	if (cur != "" && n++ == 0) { element(X, cur); cursor(Val); return; }
 	for (const e of stk) { element(n > sel ? U : (n == 1 && sel == 2) ? W : X, e.value); n--; }
-	if (cur != "") element(sel > 1 ? W : X, cur); cursor(mod); setres(res, sel);
+	if (cur != "") element(sel > 1 ? W : X, cur); cursor(cur != "" ? Val : st()); setres(res, sel);
 	window.scrollTo(0, document.body.scrollHeight);
 }
 function toggle(t, d, i) { i.className = "toggle " + (t.style.display = (getComputedStyle(t).display != d ? d : "none")); }
